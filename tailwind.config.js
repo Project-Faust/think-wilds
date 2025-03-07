de4a521d2a2064177364ad76ee1273f8fdab2587
Project-Faust <pentecost.cody.lane@gmail.com>
2025-03-06 21:12:25 -0500

    init commit

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{js,ts,jsx,tsx}"], // Ensure Tailwind scans your files
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
