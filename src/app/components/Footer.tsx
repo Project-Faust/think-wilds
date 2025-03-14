ace8d354fc48f6e3c1102702ee500fbfbef06a5f
Lane Pentecost <30759236+Project-Faust@users.noreply.github.com>
2025-03-14 18:04:51 +0000

    updated to remove copyright claim

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
     return (
         <footer className="footer sm:footer-horizontal bg-neutral text-neutral-content items-center p-4">
             <aside className="grid-flow-col items-center">
-                <p>Copyright Project-Faust © {new Date().getFullYear()} - All right reserved</p>
+                <p>Created by Project-Faust © {new Date().getFullYear()}</p>
             </aside>
             <nav className="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
             </nav>
