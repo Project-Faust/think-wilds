f2a874430b73e96337b145091092d14cb3dfc11c
Lane Pentecost <30759236+Project-Faust@users.noreply.github.com>
2025-03-14 19:37:54 +0000

    populated sample weapon data

---
+++ src/app/components/equipment/data.ts
@@ -0,0 +1,17 @@
+export const weaponTypes = [
+    "Greatsword",
+    "Longsword",
+    "Sword & Shield",
+    "Dual Blades",
+    "Hammer",
+    "Hunting Horn",
+    "Lance",
+    "Gunlance",
+    "Switch Axe",
+    "Charge Blade",
+    "Insect Glaive",
+    "Bow",
+    "Light Bowgun",
+    "Heavy Bowgun"
+]
+
