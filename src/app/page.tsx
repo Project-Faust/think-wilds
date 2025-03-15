905d76d1e5ca4078acb404b03921b0abcd1cf0e5
Project-Faust <pentecost.cody.lane@gmail.com>
2025-03-15 15:33:25 -0400

    import talisman selector componenent; minor UI changes due to removing dark mode response

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,17 @@
 import ArmorSelector from "./components/equipment/ArmorSelector";
+import TalismanSelector from "./components/equipment/TalismanSelector";
 import WeaponSelector from "./components/equipment/WeaponSelector";
 
 export default function Home() {
   return (
     <div>
-      <header className="text-center bg-black mx-0 mt-0 mb-5 p-5">
-        <h1 className="text-4xl font-bold mb-3">Think Wilds</h1>
+      <header className="text-center bg-base-100 text-white mx-0 mt-0 mb-5 p-5">
+        <h1 className="text-4xl font-bold">Think Wilds</h1>
         <p className="text-xl">A community-made set builder for Monster Hunter: Wilds</p>
       </header>
       <WeaponSelector />
       <ArmorSelector />
+      <TalismanSelector />
     </div>
   );
 }
