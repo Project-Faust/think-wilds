e162196ea27cd4a93de3a9f093097fbbe8e24aa9
Project-Faust <pentecost.cody.lane@gmail.com>
2025-03-15 16:20:38 -0400

    added daisyui theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" data-theme="dracula">
       <body className="flex flex-col min-h-screen">
         <main className="flex-grow">
           {children}
