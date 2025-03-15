a00729372cfc4bba7c3cf12aa705704a05d4bb16
Project-Faust <pentecost.cody.lane@gmail.com>
2025-03-15 16:20:56 -0400

    updated ui styling with daisyui classes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import WeaponSelector from "./components/equipment/WeaponSelector";
 export default function Home() {
   return (
     <div>
-      <header className="text-center bg-base-100 text-white mx-0 mt-0 mb-5 p-5">
+      <header className="text-center bg-base-300 text-base-content mx-0 mt-0 mb-5 p-5">
         <h1 className="text-4xl font-bold">Think Wilds</h1>
         <p className="text-xl">A community-made set builder for Monster Hunter: Wilds</p>
       </header>
