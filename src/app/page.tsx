6a158ce7dd6dc5d93806f55eac7f487f06987b57
Project-Faust <pentecost.cody.lane@gmail.com>
2025-03-15 14:30:39 -0400

    import armor and weapon select components; populate to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
+import ArmorSelector from "./components/equipment/ArmorSelector";
+import WeaponSelector from "./components/equipment/WeaponSelector";
+
 export default function Home() {
   return (
     <div>
-      <h1 className="text-3xl font-bold">Think Wilds</h1>
+      <header className="text-center bg-black mx-0 mt-0 mb-5 p-5">
+        <h1 className="text-4xl font-bold mb-3">Think Wilds</h1>
+        <p className="text-xl">A community-made set builder for Monster Hunter: Wilds</p>
+      </header>
+      <WeaponSelector />
+      <ArmorSelector />
     </div>
   );
 }
