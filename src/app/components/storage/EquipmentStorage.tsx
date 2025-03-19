91910d247866a17067b52fe3ee69e4f9668bd915
Project-Faust <pentecost.cody.lane@gmail.com>
2025-03-19 14:04:56 -0400

    updated tentatively to test toggle for conditionals

--- src/app/components/storage/EquipmentStorage.tsx
+++ src/app/components/storage/EquipmentStorage.tsx
@@ -1,37 +1,129 @@
 import { create } from "zustand";
 
 interface Skill {
-    mame: string;
+    name: string;
     level: number;
 }
 
 interface ArmorStats {
-    defense: number;
-    skills: Skill[]
-
+    id: number;
+    name: string;
+    defense: {
+        base: number;
+    }
+    resistances: {
+        fire: number;
+        water: number;
+        ice: number;
+        thunder: number;
+        dragon: number;
+    };
+    skills: Skill[];
 }
 
 interface TalismanStats {
     skills: Skill[]
 }
 
-interface EquipmentState {
-    armor: ArmorStats;
+interface BuffsState {
+    inventoryCharms: {
+        armorCharm: boolean;
+        powerCharm: boolean;
+    };
+    conditionalSkills: {
+        agitator: boolean;
+        maxMight: boolean;
+        peakPerformance: boolean;
+        heroics: boolean;
+        latentPower: boolean;
+        critElement: boolean;
+        critStatus: boolean;
+    };
+    consumableBuffs: {
+        demonDrug: boolean;
+        megaDemonDrug: boolean;
+        mightSeed: boolean;
+        mightPill: boolean;
+    };
+    toggleInventoryCharm: (charm: keyof BuffsState["inventoryCharms"]) => void;
+    toggleConditionalSkill: (skill: keyof BuffsState["conditionalSkills"]) => void;
+    toggleConsumableBuff: (buff: keyof BuffsState["consumableBuffs"]) => void;
+}
+
+interface EquipmentState extends BuffsState {
+    armor: ArmorStats | null;
     talisman: TalismanStats;
     setArmor: (armor: ArmorStats) => void;
     setTalisman: (talisman: TalismanStats) => void;
 }
 
 const useEquipmentStore = create<EquipmentState>((set) => ({
-    armor: {
-        defense: 0,
-        skills: [],
-    },
+    armor: null,
     talisman: {
         skills: [],
     },
-    setArmor: (armor) => set({ armor }),
+
+    setArmor: (armor) =>
+        set({
+            armor: {
+                id: armor.id,
+                name: armor.name,
+                defense: {
+                    base: armor.defense.base,
+                },
+                resistances: armor.resistances,
+                skills: armor.skills.map((skill: any) => ({
+                    name: skill.skillName,
+                    level: skill.level,
+                })),
+            },
+        }),
+
     setTalisman: (talisman) => set({ talisman }),
+
+    // Buffs & Conditionals
+
+    // inventory charms are permanent on a per-hunt basis
+    // state depends on if user has charms or not
+    inventoryCharms: {
+        armorCharm: false,
+        powerCharm: false,
+    },
+
+    // assumed to not be persistent throughout an entire hunt
+    // enable all applicable to see the stars align
+    conditionalSkills: {
+        agitator: false,
+        maxMight: false,
+        peakPerformance: false,
+        heroics: false,
+        latentPower: false,
+        critElement: false,
+        critStatus: false,
+    },
+    // TO-DO:
+    // each subtype should be mutually exclusive
+    consumableBuffs: {
+        demonDrug: false,
+        megaDemonDrug: false,
+        mightSeed: false,
+        mightPill: false,
+    },
+
+    toggleInventoryCharm: (charm) =>
+        set((state) => ({
+            inventoryCharms: { ...state.inventoryCharms, [charm]: !state.inventoryCharms[charm] },
+        })),
+
+    toggleConditionalSkill: (skill) =>
+        set((state) => ({
+            conditionalSkills: { ...state.conditionalSkills, [skill]: !state.conditionalSkills[skill] },
+        })),
+
+    toggleConsumableBuff: (buff) =>
+        set((state) => ({
+            consumableBuffs: { ...state.consumableBuffs, [buff]: !state.consumableBuffs[buff] },
+        })),
 }));
 
 export default useEquipmentStore;
